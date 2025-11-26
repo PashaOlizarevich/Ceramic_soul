f527f7458c7393c321a03ca638c240a809bffe26
Pasha Alizarevich <Pasha.sokol1992@icloud.com>
2025-11-26 16:11:27 +0300

    new section and section optimization

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
+            /* сторонний код  */
+/* import "purecss/build/grids-min.css";
+import "purecss/build/grids-responsive-min.css"; */
+                /* мой код */
 import "/src/sass/style.scss";
+
+
+
