5e8277c3001933e535eca8bf33ac956ca2c797cd
Pasha Alizarevich <Pasha.sokol1992@icloud.com>
2026-01-06 14:06:03 +0300

    add catalog html

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,7 @@ close.addEventListener("click", () => {
 	document.body.style.overflow = "";
 });
 
+
 try {
 	new Swiper(".works__slider", {
 		slidesPerView: 1,
@@ -45,4 +46,24 @@ try {
 		},
 		modules: [Navigation, Pagination],
 	});
+} catch (e) {}
+
+try {
+	const tabs = document.querySelectorAll(".catalog__tab");
+	const contents = document.querySelectorAll(".catalog__content-item");
+
+	tabs.forEach((tab, index) => {
+		tab.addEventListener("click", () => {
+			// Удаляем активный класс у всех табов и контента
+			tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+			contents.forEach((c) => (c.style.display = "none"));
+
+			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
+			tab.classList.add("catalog__tab_active");
+			contents[index].style.display = "block";
+		});
+	});
+
+	// Показываем первый контент при загрузке
+	contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
 } catch (e) {}
